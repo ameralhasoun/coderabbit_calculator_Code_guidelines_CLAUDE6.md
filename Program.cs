2297667486cdaab898a722e6603c955e545921a0
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-11-10 11:48:49 +0100

    add simple Calculator class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Example
+{
+    // A simple calculator class
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return checked(a + b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
